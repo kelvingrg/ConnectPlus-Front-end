53f11a1a9237a90a343e986450c67125a6a81bd8
kelvin george <kelvinpktl@gmail.com>
2022-11-15 23:02:48 +0530

    persist state is completetd successfully

--- src/App/ReduxHandlers/store.js
+++ src/App/ReduxHandlers/store.js
@@ -4,12 +4,27 @@ import modalReducer from "./ModalSlice"
 import postTypeReducer from './PostTypeReducer';
 import loginReducer from './LoginReducer';
 
+// to persist state (store is protected from refresh )
+import storage from "redux-persist/lib/storage"
+import {persistReducer} from "redux-persist" 
+import { combineReducers } from '@reduxjs/toolkit';
+
+const persistConfig={
+    key:"root",
+    version:1,
+    storage
+}
+const reducer=combineReducers({
+    modal: modalReducer,
+    postType: postTypeReducer,
+    login: loginReducer
+})
+
+const persistedReducer=persistReducer(persistConfig,reducer)
 
 export const store = configureStore({
 
-    reducer: {
-        modal: modalReducer,
-        postType: postTypeReducer,
-        login: loginReducer
-    }
+    reducer: persistedReducer
+        
+    
 });
--- src/index.js
+++ src/index.js
@@ -6,14 +6,20 @@ import { BrowserRouter } from 'react-router-dom'
 import { Provider } from 'react-redux'
 import {store} from './App/ReduxHandlers/store'
 
+import {PersistGate}  from 'redux-persist/es/integration/react' // for persist state 
+import { persistStore } from 'redux-persist';  // for persist state 
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
+
+let persistor =persistStore(store)
 root.render(
   <React.StrictMode>
     <BrowserRouter>
     <Provider store={store}>
+      <PersistGate persistor={persistor}>
     <App />
+    </PersistGate>
     </Provider>
     </BrowserRouter>
   </React.StrictMode>
